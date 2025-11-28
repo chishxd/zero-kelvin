1b7e0115e55fb431c96350de19f9f2071b002780
Chinmay Shet <chishxd@gmail.com>
2025-11-28 21:16:46 +0530

    feat: Write basic View() method

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-type model struct{
-	Aura int
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	Aura        int
 	Temperature int
-	Discipline int
-	Log []string 
+	Discipline  int
+	Log         []string
+}
+
+func (m model) View() string {
+	s := "PROJECT ZERO KELVIN"
+
+	s += fmt.Sprintf("----------\n Temperature: %d C \n Aura: %d C\n Discipline: %d C \n", m.Temperature, m.Aura, m.Discipline)
+
+	return s
 }
