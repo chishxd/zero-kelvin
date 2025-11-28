068567c3e0b720368469e582c64736238f64e3da
Chinmay Shet <chishxd@gmail.com>
2025-11-28 21:50:00 +0530

    feat: basic checkpoint boilerplate done

--- main.go
+++ main.go
@@ -10,13 +10,47 @@ type model struct {
 	Aura        int
 	Temperature int
 	Discipline  int
-	Log         []string
+	Logs        []string
 }
 
 func (m model) View() string {
-	s := "PROJECT ZERO KELVIN"
+	s := "PROJECT ZERO KELVIN\n----------\n"
 
-	s += fmt.Sprintf("----------\n Temperature: %d C \n Aura: %d C\n Discipline: %d C \n", m.Temperature, m.Aura, m.Discipline)
+	for log := range m.Logs {
+		s += fmt.Sprintf("Temperature: %d C \n Aura: %d C\n Discipline: %d C \n ----------\n Log: %s\n ", m.Temperature, m.Aura, m.Discipline, m.Logs[log])
 
+	}
+
+	s += "Press q to exit"
 	return s
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func main() {
+	initialModel := model{
+		Aura:        0,
+		Temperature: 65,
+		Discipline:  10,
+	}
+	p := tea.NewProgram(initialModel)
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Uh, oh... Seems like we have some error in the code: %v", err)
+	}
 }
